47f5f59a1bc5cdfb5ac881f5af8ecbe2de742785
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2026-06-27 03:15:24 +0800

    fix proxy ip

--- app/Config/App.php
+++ app/Config/App.php
@@ -182,8 +182,11 @@ public $negotiateLocale = false;        // biar nggak ke-override dari browser
      *
      * @var array<string, string>
      */
-    public array $proxyIPs = ['*'];
-
+    public array $proxyIPs = [
+        '127.0.0.1' => 'X-Forwarded-For',
+        '::1'       => 'X-Forwarded-For',
+    ];
+    
     /**
      * --------------------------------------------------------------------------
      * Content Security Policy
